c666823150451260d435ce7da3f2686ccffce6fe
Kristian <49764796+kristianka@users.noreply.github.com>
2023-12-27 23:54:54 +0200

    modify config

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from "cypress";
 export default defineConfig({
     e2e: {
         baseUrl: process.env.BASE_URL || "http://localhost:5173"
+    },
+    env: {
+        CYPRESS_TEST_EMAIL: process.env.CYPRESS_TEST_EMAIL
     }
 });
