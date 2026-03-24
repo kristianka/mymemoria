5640f8911c1a0051f9f7b977e5decb547c578fa9
Kristian <49764796+kristianka@users.noreply.github.com>
2026-03-25 00:35:55 +0200

    chore: fix github ci cd not supporting webgl

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -2,7 +2,17 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
     e2e: {
-        baseUrl: process.env.BASE_URL || "http://localhost:5173"
+        baseUrl: process.env.BASE_URL || "http://localhost:3000",
+        // use software rendering for WebGL in CI without GPU
+        setupNodeEvents(on, config) {
+            on("before:browser:launch", (browser, launchOptions) => {
+                if (process.env.CI && (browser.name === "chrome" || browser.name === "chromium")) {
+                    launchOptions.args.push("--use-gl=swiftshader");
+                    launchOptions.args.push("--disable-gpu-sandbox");
+                }
+                return launchOptions;
+            });
+        }
     },
     env: {
         CYPRESS_TEST_EMAIL: process.env.CYPRESS_TEST_EMAIL
